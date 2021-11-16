69e932785c9df317be2d9fd54e87b4230346d8fb
KrzysztofCiemny <ciemnykrzysztof97@gmail.com>
2021-11-17 00:58:05 +0100

    creating loop to create cards

--- js/main.js
+++ js/main.js
@@ -1,24 +1,8 @@
+import cardsImagesArray from "./cardsArray.js";
+
 document.addEventListener('DOMContentLoaded', () => {
-    const imagesArray = [
-        {
-            name: 'image1',
-            img: 'image1.jpeg'
-        },
-        {
-            name: 'image2',
-            img: 'image2.jpeg'
-        },
-        {
-            name: 'image3',
-            img: 'image3.jpeg'
-        },
-        {
-            name: 'image4',
-            img: 'image4.jpeg'
-        }
-    ];
-    const copyOfImagesArray = [...imagesArray];
-    const cardsImagesArray = [].concat(imagesArray, copyOfImagesArray);
+
 });
+
 
 
---
+++ js/showCards.js
@@ -0,0 +1,9 @@
+import cardsImagesArray from "./cardsArray.js";
+
+function showCards () {
+    for(let i=0; i<cardsImagesArray.length; i++) {
+        let card = document.createElement('img');
+        
+
+    }
+}
