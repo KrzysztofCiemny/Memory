c1a9fa8ba6da0bdd507aeb02e7728d5c260f4533
KrzysztofCiemny <ciemnykrzysztof97@gmail.com>
2021-11-16 23:11:49 +0100

    creating cardsImageArray

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    const imagesArray = [
+        {
+            name: 'image1',
+            img: 'image1.jpeg'
+        },
+        {
+            name: 'image2',
+            img: 'image2.jpeg'
+        },
+        {
+            name: 'image3',
+            img: 'image3.jpeg'
+        },
+        {
+            name: 'image4',
+            img: 'image4.jpeg'
+        }
+    ];
+    const copyOfImagesArray = [...imagesArray];
+    const cardsImagesArray = [].concat(imagesArray, copyOfImagesArray);
+});
 
 
