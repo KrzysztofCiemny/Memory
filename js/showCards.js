f902ba9f002952c959cae10c0fea9150cde169c5
KrzysztofCiemny <ciemnykrzysztof97@gmail.com>
2021-11-17 01:01:05 +0100

    setting card attributes

--- js/showCards.js
+++ js/showCards.js
@@ -3,7 +3,8 @@ import cardsImagesArray from "./cardsArray.js";
 function showCards () {
     for(let i=0; i<cardsImagesArray.length; i++) {
         let card = document.createElement('img');
-        
+        card.setAttribute('src', 'img/front.jpeg');
+        card.setAttribute('data-index', i);
 
     }
 }
