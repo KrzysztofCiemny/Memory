420d2990fc6e978a4304dcfd3fc7670ade490e6f
KrzysztofCiemny <ciemnykrzysztof97@gmail.com>
2021-11-17 01:36:44 +0100

    starting to create turnCards function

--- js/showCards.js
+++ js/showCards.js
@@ -1,14 +1,13 @@
 import cardsImagesArray from "./cardsArray.js";
+import turnCard from "./turnCards.js";
 
 const board = document.querySelector('.cards');
-console.log(cardsImagesArray);
-
-function showCards () {
+function showCards() {
     for(let i=0; i<cardsImagesArray.length; i++) {
         let card = document.createElement('img');
         card.setAttribute('src', 'img/front.jpeg');
         card.setAttribute('data-index', i);
-        // card.addEventListener('click', turnCard);
+        card.addEventListener('click', turnCard);
         board.appendChild(card);
     }
 }
---
+++ js/turnCards.js
@@ -0,0 +1,4 @@
+function turnCard() {
+
+}
+export default turnCard;
