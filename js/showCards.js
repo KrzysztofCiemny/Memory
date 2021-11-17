817ac41d8ecf0089419c56d684d6da2917c7c733
KrzysztofCiemny <ciemnykrzysztof97@gmail.com>
2021-11-17 02:14:44 +0100

    getting index of card clicked and pushing name and index to the arrays

--- js/showCards.js
+++ js/showCards.js
@@ -2,9 +2,10 @@ import cardsImagesArray from "./cardsArray.js";
 import turnCard from "./turnCards.js";
 
 const board = document.querySelector('.cards');
+
 function showCards() {
     for(let i=0; i<cardsImagesArray.length; i++) {
-        let card = document.createElement('img');
+        const card = document.createElement('img');
         card.setAttribute('src', 'img/front.jpeg');
         card.setAttribute('data-index', i);
         card.addEventListener('click', turnCard);
--- js/turnCards.js
+++ js/turnCards.js
@@ -1,4 +1,6 @@
+let cardsClicked = [];
+let cardsClickedIndex = [];
 function turnCard() {
-
+    const cardIndex = this.getAttribute('data-index');
 }
 export default turnCard;
