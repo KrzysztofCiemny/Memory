bc3ee114610e259679248b45a94dc09885ac0655
KrzysztofCiemny <ciemnykrzysztof97@gmail.com>
2021-11-17 02:27:43 +0100

    changing image from front img to one of the images

--- js/turnCards.js
+++ js/turnCards.js
@@ -1,6 +1,12 @@
+import cardsImagesArray from "./cardsArray";
+
 let cardsClicked = [];
 let cardsClickedIndex = [];
+
 function turnCard() {
     const cardIndex = this.getAttribute('data-index');
+    cardsClicked.push(cardsImagesArray[cardIndex].name);
+    cardsClickedIndex.push(cardIndex);
+    this.setAttribute('src', cardsImagesArray[cardIndex].img);
 }
 export default turnCard;
