445cacbff08e62ed2872ffcaf9c51a7aceea5ef7
KrzysztofCiemny <ciemnykrzysztof97@gmail.com>
2021-11-19 01:33:04 +0100

    creating function for match

--- js/turnCards.js
+++ js/turnCards.js
@@ -8,6 +8,27 @@ function turnCard() {
     cardsClicked.push(cardsImagesArray[cardIndex].name);
     cardsClickedIndex.push(cardIndex);
     this.setAttribute('src', cardsImagesArray[cardIndex].img);
-    // if (cardsClicked.length === 2) checkForMatches();
+    if (cardsClicked.length === 2) checkForMatches();
 }
+
+const first = 0;
+const second = 1;
+
+function checkForMatches() {
+    const cards = document.querySelectorAll('img');
+    const firstClickedCardId = cardsClickedIndex[first];
+    const secondClickedCardId = cardsClickedIndex[second];
+    console.log(cards[firstClickedCardId]);
+    if(cardsClicked[first] === cardsClicked[second]) match(cards, firstClickedCardId, secondClickedCardId);
+    else {
+
+    }
+
+}
+
+function match(cards, first, second){
+    cards[first].classList.add('card-hit');
+    cards[second].classList.add('card-hit');
+}
+
 export default turnCard;
