a46d8147854dcea01b42d82c219de79a0d64bdf6
KrzysztofCiemny <ciemnykrzysztof97@gmail.com>
2021-12-15 21:56:18 +0100

    creating flip all cards for play again function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-import addingPlayAgainEvent from "./playAgain.js";
+import playAgain from "./playAgain.js";
 import turnCard from "./turnCards.js";
 
 const cards = document.querySelectorAll(".card");
 cards.forEach(card => card.addEventListener('click', turnCard));
 
-addingPlayAgainEvent();
+const playAgainButton = document.querySelector('.play-again');
+playAgainButton.addEventListener('click', playAgain);
+
+
 
 
 
--- js/playAgain.js
+++ js/playAgain.js
@@ -1,20 +1,19 @@
 import cardsImagesArray from "./cardsArray.js";
 import turnCard from "./turnCards.js";
 
-const playAgainButton = document.querySelector('.play-again');
 const winPlate = document.querySelector('.win-plate');
+const cards = document.querySelectorAll('.card');
 
-export default function addingPlayAgainEvent() {
-    playAgainButton.addEventListener('click', playAgain);
+export default function playAgain() {
+    cardsImagesArray.sort(() => 0.5 - Math.random());
+    setTimeout(flipBackAllCards, 1000);
+    winPlate.classList.toggle('goDown');
 }
 
-function playAgain() {
-    cardsImagesArray.sort(() => 0.5 - Math.random());
-    const cards = document.querySelectorAll('img');
+function flipBackAllCards() {
     cards.forEach(card => {
-        card.setAttribute('src', 'img/front.jpeg');
+        card.classList.toggle('flip');
         card.addEventListener('click', turnCard);
         card.style.cursor = 'pointer';
     });
-    winPlate.classList.toggle('goDown');
 }
